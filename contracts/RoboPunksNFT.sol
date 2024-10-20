51bcbced473674c89e84e5784c47beedd7bd7294
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-10-21 00:48:09 +1300

    feat: Implemented smart contract deployment onto Sepolia testnet.

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract RoboPunksNFT is ERC721, Ownable {
+contract RoboPunksNFT is ERC721,Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
@@ -14,7 +14,7 @@ abstract contract RoboPunksNFT is ERC721, Ownable {
     address payable public withdrawWallet;
     mapping (address => uint256) public walletMints;
 
-    constructor() payable ERC721('RoboPunks', 'RP') {
+    constructor() payable ERC721('RoboPunks', 'RP') Ownable(msg.sender) {
         mintPrice = 0.02 ether;
         totalSupply = 0;
         maxSupply = 1000;
@@ -40,5 +40,18 @@ abstract contract RoboPunksNFT is ERC721, Ownable {
     function withdraw() external onlyOwner {
         (bool success, ) = withdrawWallet.call{value: address(this).balance}("");
         require(success, 'Withdraw failed');
+    }
+
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'Public mint is disabled');
+        require(msg.value == quantity_ * mintPrice, 'Invalid ether amount');
+        require(totalSupply + quantity_ <= maxSupply, 'Sold out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'Exceeds max per wallet');
+
+        for (uint256 i = 0; i < quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
     }
 }
---
+++ scripts/deployRoboPunksNFT.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const RoboPunksNFT = await hre.ethers.getContractFactory("RoboPunksNFT");
+  const roboPunksNFT = await RoboPunksNFT.deploy();
+
+  console.log("RoboPunksNFT deployed to:", roboPunksNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
